e878aa276bb17e83f89aa22d59f90f9fa342f517
AndreaF19 <54316921+AndreaF19@users.noreply.github.com>
2019-10-04 21:05:44 -0300

    2da Entrega (Opcional) proyecto e-commerce JAP
    Opcional:
    En la portada del sitio, agregar en la esquina superior derecha (dentro de la barra superior), el nombre del usuario ingresado en la pantalla de inicio de sesión. .
    Convierte el nombre del usuario del punto anterior, en forma de menú desplegable como puedes encontrarlo aquí, y agrega allí las opciones:
    Ver mi carrito: (recuerda quitarla desde la barra superior) y que al seleccionarla redirija a la misma pantalla.
    Cerrar sesión: deberá redirigir a la pantalla de inicio de sesión, borrando el usuario autenticado.
    Autenticación real con Google.

---
+++ js/index.js
@@ -0,0 +1,58 @@
+
+function guardarDatos() { //guarda el nombre y la contraseña
+  localStorage.nombre = document.getElementById("user").value;
+localStorage.contraseña = document.getElementById("pwd").value;
+}
+
+
+
+function UsuarioAutenticacion(){ //autentica al usuario
+   var nombre = document.getElementById("user").value;
+   var contraseña = document.getElementById("pwd").value;
+if (nombre=="Andrea" && contraseña=="1234"){
+  
+window.location.href = "mainpage.html";// Redirige a la pagina principal.
+
+}
+
+else{
+   
+return alert("Algunos de los campos igresados no es correcto o hay campos vacios, por favor verifique");
+}
+}
+
+function mostrarNombreUsuario() {
+  if ((localStorage.nombre != undefined) && (localStorage.contraseña != undefined)) {
+       document.getElementById("user").innerHTML = localStorage.nombre ;
+   } 
+}
+
+function BorrarDatos(){
+   localStorage.removeItem ('nombre');
+   localStorage.removeItem ('contraseña');
+
+}
+
+  //AUTENTICACION CON GOOGLE
+
+  function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    var id_token = googleUser.getAuthResponse().id_token;
+    console.log(id_token);
+    window.location.replace("mainpage.html");// Redirige a la pagina principal.
+
+  }
+    function signOut() {
+        var auth2 = gapi.auth2.getAuthInstance();
+        auth2.signOut().then(function () {
+          console.log('User signed out.');
+        
+         
+        });
+      }
+  
+  
