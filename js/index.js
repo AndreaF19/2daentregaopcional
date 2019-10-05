63c2a7c0acfe18b0878b59f217e1ab615c89d0fb
AndreaF19 <54316921+AndreaF19@users.noreply.github.com>
2019-10-05 15:38:18 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -49,7 +49,7 @@ function BorrarDatos(){
   function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
-        auth2.disconnect();
+      window.location.href ="https://appengine.google.com/_ah/logout?continue=http://https://andreaf19.github.io/2daentregaopcional/";
         console.log('User signed out.');
 
     });
@@ -57,7 +57,7 @@ function BorrarDatos(){
   function onLoad() {
     gapi.load('auth2', function() {
       gapi.auth2.init()
-    window.location.href ="https://appengine.google.com/_ah/logout?continue=http://https://andreaf19.github.io/2daentregaopcional/";
+  
     });
   }
 
