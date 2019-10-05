983d7d673ff6c4665f44031009123b5161ca41cf
AndreaF19 <54316921+AndreaF19@users.noreply.github.com>
2019-10-05 15:31:18 -0300

    2da Entrega (Opcional) proyecto e-commerce JAP

--- js/index.js
+++ js/index.js
@@ -50,6 +50,8 @@ function BorrarDatos(){
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
         auth2.disconnect();
+        console.log('User signed out.');
+
     });
   }
   function onLoad() {
