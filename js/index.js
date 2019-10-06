5273100c4fb60b5886bff7191eb988ad787b0221
AndreaF19 <54316921+AndreaF19@users.noreply.github.com>
2019-10-06 18:08:13 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -51,22 +51,19 @@ function BorrarDatos(){
     window.location.href="mainpage.html";// Redirige a la pagina principal.
    
   }
-  function init() {
-    gapi.load('auth2', function() {
-        gapi.auth2.init({ client_id: '485493375484-pi1k1ntjhmlcem9kcts91liah5cpp4tf.apps.googleusercontent.com' })
-     });
-   }
-   var auth2;
-  var gapi;
-  gapi.auth2.init().then(()=>{
-  singOut = function () {
-   var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      auth2.disconnect();
-   
-     console.log('User signed out.');  
+  
+  gapi.load('auth2', initSigninV2);
 
-});
+function initSigninV2() {
+    gapi.auth2.init({
+        client_id: '485493375484-pi1k1ntjhmlcem9kcts91liah5cpp4tf.apps.googleusercontent.com'
+    }).then(function (_authInstance) {
+        // now auth2 is fully initialized
+    });
 }
-})
-
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    console.log('User signed out.');
+  });
+}
