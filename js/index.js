61088066a5f6c00280679ba023c19f8f850d7421
AndreaF19 <54316921+AndreaF19@users.noreply.github.com>
2019-10-06 19:29:23 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -56,11 +56,14 @@ function BorrarDatos(){
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
       console.log('User signed out.');
-    });
+    });    
+    auth2.disconnect();
+
   }
 
   function onLoad() {
     gapi.load('auth2', function() {
       gapi.auth2.init();
     });
+    
   }
