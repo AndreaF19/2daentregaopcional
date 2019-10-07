ae5a79fc0108c95befda1369e6a34b33fe7336cc
AndreaF19 <54316921+AndreaF19@users.noreply.github.com>
2019-10-06 22:02:59 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -49,13 +49,17 @@ function BorrarDatos(){
     var id_token = googleUser.getAuthResponse().id_token;
     console.log("ID Token: " + id_token);
     window.location.href="mainpage.html";// Redirige a la pagina principal.
-   
   }
+   function disconnect(){
+    location.href="https://www.google.com/accounts/Logout";
+   }
+
+  
   function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
       console.log('User signed out.');
-      location.href="https://www.google.com/accounts/Logout"
+      auth2.disconnect();
     });    
     auth2.disconnect();
 
